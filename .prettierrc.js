772c2e9ec5039ac6d0f308b749aff4918e372bc3
zhangzy56 <doxoops@163.com>
2024-01-02 16:31:52 +0800

    fix: prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,8 @@
 module.exports = {
   singleQuote: true,
-  semi: false,
+  semi: false, // 在语句末尾添加分号
   printWidth: 120,
-  tabWidth: 2,
-  arrowParens: 'avoid',
-  bracketSpacing: true,
-  trailingComma: 'none',
-  proseWrap: 'never'
-  // singleAttributePerLine: true // 在 Vue 中对每个属性进行自动换行
+  arrowParens: 'avoid', // 尽可能省略箭头函数括号
+  trailingComma: 'none', // 不需要尾随逗号
+  // singleAttributePerLine: true // 在 HTML、Vue 和 JSX 中对每个属性进行自动换行
 }
