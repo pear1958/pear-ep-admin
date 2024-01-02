5cad2f85c3209280431dd8b5d747ea987e68b621
zhangzy56 <doxoops@163.com>
2024-01-02 16:48:00 +0800

    fix: prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,5 +4,6 @@ module.exports = {
   printWidth: 120,
   arrowParens: 'avoid', // 尽可能省略箭头函数括号
   trailingComma: 'none', // 不需要尾随逗号
+  htmlWhitespaceSensitivity: 'ignore' // 去除 html、Vue、Handlebars 中的多余空格
   // singleAttributePerLine: true // 在 HTML、Vue 和 JSX 中对每个属性进行自动换行
 }
