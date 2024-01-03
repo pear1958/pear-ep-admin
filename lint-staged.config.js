05566ad0bf1ca87b6f04c3fc8e4532d46be520d9
zhangzy56 <doxoops@163.com>
2024-01-04 00:53:14 +0800

    fix: file-name

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
-module.exports =  {
+module.exports = {
   '*.{js,jsx,ts,tsx}': ['prettier --write'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
   'package.json': ['prettier --write'],
