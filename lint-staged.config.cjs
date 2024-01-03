77918b1fd930aced4710207a4a5832a217bb6c93
zhangzy56 <doxoops@163.com>
2024-01-03 12:36:31 +0800

    feat: 添加husky lint-staged prettier

---
+++ lint-staged.config.cjs
@@ -0,0 +1,8 @@
+module.exports =  {
+  '*.{js,jsx,ts,tsx}': ['prettier --write'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
+  'package.json': ['prettier --write'],
+  '*.vue': ['prettier --write'],
+  '*.{scss,less,styl,html}': ['prettier --write'],
+  '*.md': ['prettier --write']
+}
