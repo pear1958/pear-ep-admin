295d40aff138c7155756eb556ed827c2916d5743
zhangzy56 <doxoops@163.com>
2024-01-04 12:27:31 +0800

    fix: lint

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,9 +1,9 @@
-export const a = "2222";
+export const a = '2222'
 
 export const test = () => {
-  console.log("465121");
+  console.log('465121')
 }
 
 export const test2 = () => {
-  console.log("test2---xx--");
+  console.log('test2---xx77777--')
 }
