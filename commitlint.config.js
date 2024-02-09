abc113d0de1925bf3d4bd492e561697435654870
zhangzy56 <doxoops@163.com>
2024-02-10 02:13:00 +0800

    fix: 配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -56,10 +56,8 @@ export default {
       scope: '选择一个提交范围（可选）:',
       customScope: '请输入自定义的提交范围 :',
       subject: '填写简短精炼的变更描述 :\n',
-      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-      generatingByAI: '正在通过 AI 生成你的提交简短描述...',
-      generatedSelectByAI: '选择一个 AI 生成的简短描述:',
+      body: '填写更加详细的变更描述（可选1）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选ff）。使用 "|" 换行 :\n',
       confirmCommit: '是否提交或修改commit ?'
     },
     types: [
