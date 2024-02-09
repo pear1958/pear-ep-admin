a796a6f26e576717f45fad89b77d19993b0a12e2
zhangzy56 <doxoops@163.com>
2024-02-09 16:20:19 +0800

    feat: 增加commitLint效验

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 export default {
   singleQuote: true,
   semi: false, // 在语句末尾添加分号
-  printWidth: 120,
+  printWidth: 80,
   arrowParens: 'avoid', // 尽可能省略箭头函数括号
   trailingComma: 'none', // 不需要尾随逗号
-  htmlWhitespaceSensitivity: 'ignore' // 去除 html、Vue、Handlebars 中的多余空格
-  // singleAttributePerLine: true // 在 HTML、Vue 和 JSX 中对每个属性进行自动换行
+  htmlWhitespaceSensitivity: 'ignore', // 去除 html、Vue、Handlebars 中的多余空格
+  singleAttributePerLine: true // 在 HTML、Vue 和 JSX 中对每个属性进行自动换行
 }
