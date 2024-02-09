600a80637e9d115d89afc3c24663d63335c3aefe
zhangzy56 <doxoops@163.com>
2024-02-09 20:30:33 +0800

    feat: 新增git-cz类型
    新增了几个type
    
    比如: wip workflow types release

--- commitlint.config.js
+++ commitlint.config.js
@@ -67,17 +67,17 @@ export default {
     types: [
       { value: 'feat', name: 'feat: 新增功能' },
       { value: 'fix', name: 'fix: 修复缺陷' },
-      { value: 'docs', name: 'docs: 文档变更' },
+      { value: 'perf', name: 'perf: 性能优化' },
       {
         value: 'style',
         name: 'style: 代码格式（不影响功能，例如空格、分号等格式修正）'
       },
+      { value: 'docs', name: 'docs: 文档变更' },
+      { value: 'test', name: 'test: 添加疏漏测试或已有测试改动' },
       {
         value: 'refactor',
         name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
       },
-      { value: 'perf', name: 'perf: 性能优化' },
-      { value: 'test', name: 'test: 添加疏漏测试或已有测试改动' },
       {
         value: 'build',
         name: 'build: 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
@@ -87,6 +87,26 @@ export default {
       {
         value: 'chore',
         name: 'chore: 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
+      },
+      {
+        value: 'revert',
+        name: 'revert: 回退'
+      },
+      {
+        value: 'wip',
+        name: 'wip: 开发中'
+      },
+      {
+        value: 'workflow',
+        name: 'workflow: 工作流相关文件修改'
+      },
+      {
+        value: 'types',
+        name: 'types: 类型定义文件更改'
+      },
+      {
+        value: 'release',
+        name: 'release: 发布新版本'
       }
     ]
   }
