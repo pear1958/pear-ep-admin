d3bff732eefbe49a65de4123187c3c557011ff41
zhangzy56 <doxoops@163.com>
2024-02-10 19:02:09 +0800

    fix: code

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 export default {
   '*.{js,jsx,ts,tsx,cjs}': ['prettier --write'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-    'prettier --write--parser json'
-  ],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
   'package.json': ['prettier --write'],
   '*.vue': ['prettier --write'],
   '*.{scss,less,styl,html}': ['prettier --write'],
