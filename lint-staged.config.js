48acc21a334cf69f7426df3a9158a4533b6c172d
zhangzy56 <doxoops@163.com>
2024-02-10 17:41:02 +0800

    feat: 添加eslint的初始化配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,8 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['prettier --write'],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --write--parser json'],
+  '*.{js,jsx,ts,tsx,cjs}': ['prettier --write'],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+    'prettier --write--parser json'
+  ],
   'package.json': ['prettier --write'],
   '*.vue': ['prettier --write'],
   '*.{scss,less,styl,html}': ['prettier --write'],
