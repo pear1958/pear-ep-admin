de59e89d1fbcf550290d2b5fccfb89daa262dc32
zhangzy <zhangzhiyong@klicen.com>
2024-09-14 16:46:38 +0800

    feat: 优化pinia

--- src/store/utils/persist.ts
+++ src/store/utils/persist.ts
@@ -1,9 +1,14 @@
 import { PersistedStateOptions } from 'pinia-plugin-persistedstate'
 
-const piniaPersistConfig = (key: string, paths?: string[]) => {
+const obj = {
+  local: localStorage,
+  session: sessionStorage
+}
+
+const piniaPersistConfig = (key: string, storage: keyof typeof obj = 'local', paths?: string[]) => {
   const persist: PersistedStateOptions = {
     key,
-    storage: localStorage,
+    storage: obj[storage],
     paths // 默认值为 undefined
   }
 
